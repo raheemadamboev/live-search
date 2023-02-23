d8d2666df03034a8efec4fa85250f057c48a0365
raheemadamboev <vividricemaryandmax@gmail.com>
2023-02-23 18:19:04 +0500

    Added artificial in memory people database;

---
+++ app/src/main/java/xyz/teamgravity/livesearch/PersonDatabase.kt
@@ -0,0 +1,26 @@
+package xyz.teamgravity.livesearch
+
+object PersonDatabase {
+    val value = listOf(
+        PersonModel(
+            firstName = "Raheem",
+            lastName = "Adam"
+        ),
+        PersonModel(
+            firstName = "Cristiano",
+            lastName = "Ronaldo"
+        ),
+        PersonModel(
+            firstName = "Andrew",
+            lastName = "Tate"
+        ),
+        PersonModel(
+            firstName = "Elon",
+            lastName = "Musk"
+        ),
+        PersonModel(
+            firstName = "Tupac",
+            lastName = "Shakur"
+        )
+    )
+}
